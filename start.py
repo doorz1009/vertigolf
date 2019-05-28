13d818f23b7e6b82793f7b62b7fbf5143d12fb9a
Ryan <doorz1009@gmail.com>
2019-05-28 14:18:20 -0400

    Added different layers for different purposes

--- start.py
+++ start.py
@@ -2,14 +2,15 @@ import cocos
 import csv
 import pyglet
 
-cocos.director.director.init(width=1024, height=768)
+bgLayer = cocos.layer.Layer()
+gameLayer = cocos.layer.Layer()
 
 layer = cocos.layer.Layer()
 
 bg = pyglet.resource.image('Resources/golf_course.png')
 bg_sprite = cocos.sprite.Sprite(bg)
 bg_sprite.scale = 0.75
-layer.add(bg_sprite)
+bgLayer.add(bg_sprite)
 
 ball = pyglet.resource.image('Resources/golf_ball.png')
 ball_sprite = cocos.sprite.Sprite(ball)
@@ -25,9 +26,9 @@ with open('level.dat') as csv_file:
         grass_sprite.position = int(row[0]), int(row[1])
         grass_sprite.rotation = int(row[2])
         grass_sprite.scale = 0.25
-        layer.add(grass_sprite)
+        gameLayer.add(grass_sprite)
 
-layer.add(ball_sprite)
+gameLayer.add(ball_sprite)
 
-main_scene = cocos.scene.Scene (layer)
+main_scene = cocos.scene.Scene (bgLayer, gameLayer)
 cocos.director.director.run (main_scene)
